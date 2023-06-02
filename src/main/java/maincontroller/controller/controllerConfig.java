a58f75484818cbaa80fa7902340aa4b0608a35d2
Abdelrahman Hegab <prenshegab@gmail.com>
2023-06-02 21:48:50 +0300

    handeling correct routing logic

--- src/main/java/maincontroller/controller/controllerConfig.java
+++ src/main/java/maincontroller/controller/controllerConfig.java
@@ -1,23 +1,15 @@
 package maincontroller.controller;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import jakarta.servlet.http.HttpServletRequest;
 import maincontroller.mqconfig.MQSender;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @EnableRabbit
 @RestController
@@ -36,66 +28,102 @@ public class controllerConfig {
     }
 
     @PostMapping("/{microserviceName}/**")
-    public String routeRequest(@PathVariable String microserviceName, @RequestBody String request) {
+    public String routeRequest(@PathVariable String microserviceName, @RequestBody String requestBody, HttpServletRequest httpRequest) {
+
+        Gson gson = new Gson();
+        String URI = httpRequest.getRequestURI();
         String routingKey = microserviceName.substring(0,microserviceName.length()-1) + "Sender";
+        String route = microserviceName.substring(0,microserviceName.length()-1) + URI.substring(5 + microserviceName.length());
+        String method = "Post";
+        JsonObject body =  gson.fromJson(requestBody, JsonObject.class);
+        JsonObject jsonRequest = new JsonObject();
+
+        System.out.println(body+"body");
+
+        jsonRequest.addProperty("correlationId", "1234");
+        jsonRequest.addProperty("method", method);
+        jsonRequest.addProperty("route", route);
+        jsonRequest.add("body", body);
+
+
         int instance= Integer.parseInt(microserviceName.substring(microserviceName.length()-1));
         String requestId = java.util.UUID.randomUUID().toString();
-//        String jsonRequest = convertRequestToJson(request,requestId);
-//        Gson gson = new GsonBuilder().serializeSpecialFloatingPointValues().create();
-//          String request2= gson.toJson(request);
-//        JsonObject request2 = JsonParser.parseString(request).getAsJsonObject();
-          System.out.println(request+"request2");
-//        com.google.gson.JsonObject request2 = gson.fromJson(request, com.google.gson.JsonObject.class);
-
-//        JsonObject jsonRequest= new Gson().fromJson(request, JsonObject.class);
-//        jsonRequest.addProperty("correlationId", requestId);
-        String response =rabbitTemplate.convertSendAndReceiveAsType("portfolioSender", request,new ParameterizedTypeReference<String>() {}).toString();
+          System.out.println(requestBody+"request2");
+        String response =rabbitTemplate.convertSendAndReceiveAsType(routingKey, jsonRequest.toString(),new ParameterizedTypeReference<String>() {});
         System.out.println(response+"response");
         return response;
-        //        rabbitTemplate.convertAndSend(routingKey, jsonRequest);
+
     }
 
-        private String convertRequestToJson(HttpServletRequest request, String requestId) {
-        // Read the request body
-        StringBuilder requestBody = new StringBuilder();
-        try (BufferedReader reader = request.getReader()) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                requestBody.append(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    @GetMapping("/{microserviceName}/**")
+    public String routeGetRequest(@PathVariable String microserviceName, HttpServletRequest httpRequest){
 
-        // Convert the request body to a map
-        ObjectMapper objectMapper = new ObjectMapper();
-        Map<String, Object> requestBodyMap = new HashMap<>();
-        try {
-            requestBodyMap = objectMapper.readValue(requestBody.toString(), Map.class);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        String URI = httpRequest.getRequestURI();
+        String route = microserviceName.substring(0,microserviceName.length()-1) + URI.substring(5 + microserviceName.length());
 
-        // Extract the required attributes
-        String correlationId = (String) requestBodyMap.get("correlationId");
-        String method = (String) requestBodyMap.get("method");
-        String route = (String) requestBodyMap.get("route");
-        Object body = requestBodyMap.get("body");
+        String method = "Get";
+        String correlationId = "1234";
+        JsonObject body = new JsonObject();
+        JsonObject jsonRequest = new JsonObject();
 
-        // Create a map to store request properties
-        Map<String, Object> requestData = new HashMap<>();
-        requestData.put("correlationId", requestId);
-        requestData.put("method", method);
-        requestData.put("route", route);
-        requestData.put("body", body);
+        jsonRequest.addProperty("method", method);
+        jsonRequest.addProperty("route", route);
+        jsonRequest.addProperty("correlationId", correlationId);
+        jsonRequest.add("body", body);
 
-        // Convert the map to JSON string
-        try {
-            return objectMapper.writeValueAsString(requestData);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
+        String routingKey = microserviceName.substring(0,microserviceName.length()-1) + "Sender";
+        String response =rabbitTemplate.convertSendAndReceiveAsType(routingKey, jsonRequest.toString(),new ParameterizedTypeReference<String>() {});
+        System.out.println(response+"response");
+        return response;
+
     }
+
+    @DeleteMapping("/{microserviceName}/**")
+    public String routeDeleteRequest(@PathVariable String microserviceName, HttpServletRequest httpRequest){
+        String URI = httpRequest.getRequestURI();
+        String route = microserviceName.substring(0,microserviceName.length()-1) + URI.substring(5 + microserviceName.length());
+
+        String method = "Delete";
+        String correlationId = "1234";
+        JsonObject body = new JsonObject();
+        JsonObject jsonRequest = new JsonObject();
+
+        jsonRequest.addProperty("method", method);
+        jsonRequest.addProperty("route", route);
+        jsonRequest.addProperty("correlationId", correlationId);
+        jsonRequest.add("body", body);
+
+        String routingKey = microserviceName.substring(0,microserviceName.length()-1) + "Sender";
+        String response =rabbitTemplate.convertSendAndReceiveAsType(routingKey, jsonRequest.toString(),new ParameterizedTypeReference<String>() {});
+        System.out.println(response+"response");
+        return response;
+    }
+
+    @PutMapping("/{microserviceName}/**")
+    public String routePutRequest(@PathVariable String microserviceName,@RequestBody String requestBody, HttpServletRequest httpRequest){
+        Gson gson = new Gson();
+        String URI = httpRequest.getRequestURI();
+        String routingKey = microserviceName.substring(0,microserviceName.length()-1) + "Sender";
+        String route = microserviceName.substring(0,microserviceName.length()-1) + URI.substring(5 + microserviceName.length());
+        String method = "Put";
+        JsonObject body =  gson.fromJson(requestBody, JsonObject.class);
+        JsonObject jsonRequest = new JsonObject();
+
+        System.out.println(body+"body");
+
+        jsonRequest.addProperty("correlationId", "1234");
+        jsonRequest.addProperty("method", method);
+        jsonRequest.addProperty("route", route);
+        jsonRequest.add("body", body);
+
+
+        int instance= Integer.parseInt(microserviceName.substring(microserviceName.length()-1));
+        String requestId = java.util.UUID.randomUUID().toString();
+        System.out.println(requestBody+"request2");
+        String response =rabbitTemplate.convertSendAndReceiveAsType(routingKey, jsonRequest.toString(),new ParameterizedTypeReference<String>() {});
+        System.out.println(response+"response");
+        return response;
+    }
+
 
 }
